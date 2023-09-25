1d2dc9eb4d925218dca850a2debcd4fc5c583e1c
Thongek Wogponom <thongek.wpm@gmail.com>
2023-09-25 12:26:45 +0700

    commit updata

--- server/index.js
+++ server/index.js
@@ -33,3 +33,18 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+/*ROUTES WITH FIELS */
+app.post("/auth/register", upload.single("picture"), register);
+
+/* MONGOOSE SETING */
+const PORT = process.env.PORT || 3000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
