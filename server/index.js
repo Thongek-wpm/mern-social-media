bccfc751444d2265198a7507e8c42eaa966f57c7
Thongek Wogponom <thongek.wpm@gmail.com>
2023-09-27 14:31:28 +0700

    commit update

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
+import PostRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
@@ -43,6 +44,7 @@ app.post("/auth/register", upload.single("picture"), register);
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", PostRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
