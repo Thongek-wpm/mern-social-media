ecb5142ee474877f6fd4b0bcbdad898b37531437
Thongek Wogponom <thongek.wpm@gmail.com>
2023-09-29 14:05:17 +0700

    commit update

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import HomePage from "views/homepage";
-import LoginPage from "views/loginpage";
-import ProfilePage from "views/profilepage";
+import HomePage from "./views/homepage/index.jsx";
+import LoginPage from "./views/loginpage/Index.jsx";
+import ProfilePage from "./views/profilepage/index.jsx";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
